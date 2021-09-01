2b2659794f3203fbacb6e078ef4ab298ced6509d
Erfan Soleymani <80239127+erfansoleymanid@users.noreply.github.com>
2021-09-01 17:20:05 +0430

    update with swagger ui

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -18,9 +18,13 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+
 
 from personal.views import (
-	home_screen_view,
+    home_screen_view,
 )
 
 from account.views import (
@@ -40,28 +44,51 @@ urlpatterns = [
     path('logout/', logout_view, name="logout"),
     path('must_authenticate/', must_authenticate_view, name="must_authenticate"),
     path('register/', registration_view, name="register"),
-	
-	# REST-framework
+    path(
+        "openapi/",
+        login_required(
+            get_schema_view(
+                title="Service", description="Golf API developers", public=True
+            )
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "docs/",
+        login_required(
+            TemplateView.as_view(
+                template_name="documentation.html",
+                extra_context={"schema_url": "openapi-schema"},
+            )
+        ),
+        name="swagger-ui",
+    ),
+
+    # REST-framework
     path('api/blog/', include('blog.api.urls', 'blog_api')),
     path('api/account/', include('account.api.urls', 'account_api')),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
-        name='password_change_done'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
 
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), 
-        name='password_change'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
 
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-     name='password_reset_done'),
+         name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-     name='password_reset_complete'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
